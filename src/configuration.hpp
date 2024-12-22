e9c0381614e0b98bf0f18cd74afe22b896910baa
Richard SARTORI <richard.sartori223@orange.fr>
2024-12-22 14:12:03 +0100

    fix configuration

--- src/configuration.hpp
+++ src/configuration.hpp
@@ -8,25 +8,28 @@
 namespace conf {
 
 	namespace window {
-		static constexpr char const* NAME = "Stars";
-		static sf::VideoMode const MODE = { 2560, 1440, 24 };
-		static sf::Vector2u const SIZE = { MODE.width, MODE.height };
-		static sf::Vector2f const SIZE_F = static_cast<sf::Vector2f>(SIZE);
-		static sf::Vector2f const CENTER_F = SIZE_F / 2.0f;
+		inline constexpr char const NAME[] = "Stars";
+		inline constexpr unsigned int WIDTH = 2560;
+		inline constexpr unsigned int HEIGHT = 1440;
+		inline constexpr unsigned int BITS_PER_PIXEL = 24;
+		inline sf::VideoMode const MODE = { WIDTH, HEIGHT, BITS_PER_PIXEL };
+		inline sf::Vector2u const SIZE = { WIDTH, HEIGHT };
+		inline sf::Vector2f const SIZE_F = static_cast<sf::Vector2f>(SIZE);
+		inline sf::Vector2f const CENTER_F = SIZE_F / 2.0f;
 	}
 
 	namespace time {
-		static constexpr uint32_t MAX_FPS = 144;
-		static constexpr float DELTA = 1.0f / static_cast<float>(MAX_FPS);
+		inline constexpr uint32_t MAX_FPS = 144;
+		inline constexpr float DELTA = 1.0f / static_cast<float>(MAX_FPS);
 	}
 
 	namespace stars {
-		static constexpr std::size_t COUNT = 100000;
-		static constexpr float RADIUS = 40.0f;
-		static constexpr float FAR = 10.0f;
-		static constexpr float NEAR = 0.1f;
-		static constexpr float SPEED = 1.0f;
-		static constexpr std::size_t MAX_GEN_FAILS = 10;
-		static constexpr char const* TEXTURE_PATH = "textures/star.png";
+		inline constexpr std::size_t COUNT = 100000;
+		inline constexpr float RADIUS = 40.0f;
+		inline constexpr float FAR = 10.0f;
+		inline constexpr float NEAR = 0.1f;
+		inline constexpr float SPEED = 1.0f;
+		inline constexpr std::size_t MAX_GEN_FAILS = 10;
+		inline constexpr char const TEXTURE_PATH[] = "textures/star.png";
 	}
 }
